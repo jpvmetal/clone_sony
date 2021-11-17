550246cc15b4d10e5b11b13dad80741ab55af7c5
Metal <jpvmetal@gmail.com>
2021-11-17 19:24:35 -0300

    initial commit

---
+++ js/owl/setup-9.js
@@ -0,0 +1,11 @@
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:5,
+    loop:true, 
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000, 
+    autoplayHoverPause:true
+});
+
